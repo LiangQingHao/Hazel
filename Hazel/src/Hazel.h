cea549ce3a743567511a46990722fe165a635da2
liangqinghao <liangqinghao.gx.chinaccs.cn>
2023-03-10 14:42:19 +0800

    setup application and enderpoint

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// for use by Hazel application
+
+#include "Hazel/Application.h"
+
+#include "Hazel/EnterPoint.h"
---
+++ Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,23 @@
+#include <Hazel.h>
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox();
+	~Sandbox();
+
+private:
+
+};
+
+Sandbox::Sandbox()
+{
+}
+
+Sandbox::~Sandbox()
+{
+}
+
+Hazel::Application* Hazel::CreateApplication() {
+	return new Sandbox();
+}
